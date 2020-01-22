060af14cca369c9efeb77518c5589334b8da3366
Michael Davis <mdavis@mdavis-air.local>
2020-01-21 17:21:17 -0800

    class5

---
+++ Class5/SOL_tempconverter.py
@@ -0,0 +1,4 @@
+F = 35
+# formula converting F -> C, rounded to two decimal places
+C = 
+print(F + " degrees in Fahrenheit is equivalent to <C> degrees in Celsius.")
---
+++ Class5/scripting_demo_2.py
@@ -0,0 +1,18 @@
+a = 'Mike'
+b = 'Joe'
+print(a+b)
+print(a)
+
+
+a = 2
+b = 3
+d = 4
+c = a + b + d
+print(c)
+
+
+print(a
+	)
+
+
+print(f)
